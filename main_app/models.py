5fffb4734a2efb73c6b2f14793a954fc736ca7c5
Yaqoob <yaqoob.buhejji@gmail.com>
2025-09-02 14:21:03 +0300

    adding the models

--- main_app/models.py
+++ main_app/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Place(models.Model):
+    name=models.CharField(max_length=80)
+    location = models.CharField(max_length=80)
+    information = models.CharField(max_length=100)
+    review = models.CharField(max_length=80)
+    imgs = models
