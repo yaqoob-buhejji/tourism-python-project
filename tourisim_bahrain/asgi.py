ab0b8ed115d7d3c3517b99e958bcde943f0799ed
Yaqoob <yaqoob.buhejji@gmail.com>
2025-09-02 13:09:36 +0300

    Adding setup

---
+++ tourisim_bahrain/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tourisim_bahrain project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tourisim_bahrain.settings')
+
+application = get_asgi_application()
